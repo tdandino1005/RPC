575f68b3d57ac48f0576ccebb931b47012facf7c
Tony Dandino <tdandino1005@gmail.com>
2022-10-24 21:16:44 -0500

    console log every button

---
+++ src/index.js
@@ -0,0 +1,15 @@
+// TODO:make a reference to the first button on the page
+const buttons = document.querySelectorAll("button");
+
+// TODO: Do something whenever a button is clicked
+
+buttons.forEach(function (button) {
+  button.addEventListener(
+    "click",
+
+    // Callback function
+    function () {
+      console.log("Button clicked!");
+    }
+  );
+});
